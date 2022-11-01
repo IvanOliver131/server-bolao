b88194942ff166a5174460883741cb0d326e7993
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-10-31 21:38:29 -0300

    class 1: finish

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,28 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+  log: ["query"],
+});
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   });
 
-  fastify.get("/pools/count", () => {
-    return { count: 0 };
+  // Depois devemos mudar para a URL onde ficara hospedado o frontend
+  await fastify.register(cors, {
+    origin: true,
   });
 
-  await fastify.listen({ port: 3333 });
+  fastify.get("/pools/count", async () => {
+    const pools = await prisma.pool.count();
+
+    return pools;
+  });
+
+  await fastify.listen({ port: 3333, host: "0.0.0.0" });
 }
 
 bootstrap();

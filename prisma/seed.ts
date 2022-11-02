ee201fee9e7329a8c3c64e89c1bbe20d4123da0e
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-11-02 12:40:56 -0300

    feat: seed

---
+++ prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({});
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "John Doe",
+      email: "john.doe@gmail.com",
+      avatarUrl: "https://github.com/IvanOliver131.png",
+    },
+  });
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: "Example pool",
+      code: "POOL123",
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id,
+        },
+      },
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-02T18:00:00.201Z",
+      firstTeamCountryCode: "DE",
+      secondTeamCountryCode: "BR",
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-03T18:00:00.201Z",
+      firstTeamCountryCode: "BR",
+      secondTeamCountryCode: "AR",
+
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoint: 1,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+}
+
+main();
